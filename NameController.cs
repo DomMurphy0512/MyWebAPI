e5ee88cf058b71041542453cbdc09827d1f68290
DomMurphy0512 <murph2da@mail.uc.edu>
2024-07-09 19:48:28 -0400

    Add files via upload

---
+++ NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            var name = new { Name = "Domenic Murphy" };
+            return Ok(name);
+        }
+    }
+}
